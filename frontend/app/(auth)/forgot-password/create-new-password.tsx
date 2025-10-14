b546b82ef28232e3a600984e65d85321d63921d3
Cel3stt <mariannecelest@gmail.com>
2025-10-15 01:50:33 +0800

    Added create new password screen with password strength meter and verification code

--- frontend/app/(auth)/forgot-password/create-new-password.tsx
+++ frontend/app/(auth)/forgot-password/create-new-password.tsx
@@ -1,13 +1,97 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { router } from 'expo-router';
+import { Text } from '@/components/ui/text';
+import { PasswordStrengthMeter } from '@/components/ui/password-stength-meter';
 
 export default function CreateNewPassword() {
+
+  const [password, setPassword] = useState('');
   return (
-    <SafeAreaView className='justify-center items-center flex-1'>
-       <View>
-         <Text>Create New Password</Text>
-       </View>
-     </SafeAreaView>
-  )
+    <SafeAreaView className="flex-1">
+      {/* Hills image as background */}
+
+      {/* Card overlay that moves up with keyboard */}
+      <KeyboardAvoidingView
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: 1, // above the image
+        }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <View className="w-full flex-1 items-center justify-center px-4">
+          <Card
+            className="border-0 shadow-none sm:border-border sm:shadow-sm sm:shadow-black/5"
+            style={{ zIndex: 2, elevation: 5 }}>
+            <CardHeader>
+              <CardTitle className="text-center text-2xl font-semibold text-primary sm:text-left">
+                Create New Password
+              </CardTitle>
+              <CardDescription className="text-center text-base sm:text-left">
+                Create a new secure password for your account.
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent className="gap-6">
+              <View className="gap-6">
+                 {/* Password */}
+                <View className="gap-1">
+                  <Label className="text-muted-foreground font-normal">
+                    Password
+                  </Label>
+                  <Input
+                    placeholder="•••••••••"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                    // ref={passwordInputRef}
+                    returnKeyType="send"
+                    // onSubmitEditing={onSubmit}
+                    className="border-muted-foreground/50 text-primary text-xl h-12"
+                  />
+                 
+                </View>
+
+                {/* Confirm Password */}
+                <View className="gap-1">
+                  <Label className="text-muted-foreground font-normal">
+                    Confirm Password
+                  </Label>
+                  <Input
+                    placeholder="•••••••••"
+                    secureTextEntry
+                    returnKeyType="send"
+                    // onSubmitEditing={onSubmit}
+                    className="border-muted-foreground/50 text-primary text-xl h-12 mb-6"
+
+                  />
+                   <PasswordStrengthMeter password={password} />
+                </View>
+
+
+                <View className="gap-2 pt-1">
+                  <Button>
+                    <Text>Send Verification Code</Text>
+                  </Button>
+
+                  <Button
+                    variant="outline"
+                    className="mx-auto w-full"
+                    onPress={() => router.back()}>
+                    <Text className="">Cancel</Text>
+                  </Button>
+                </View>
+              </View>
+            </CardContent>
+          </Card>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
--- frontend/app/(auth)/forgot-password/verification-code.tsx
+++ frontend/app/(auth)/forgot-password/verification-code.tsx
@@ -1,6 +1,6 @@
-import { View,KeyboardAvoidingView, Platform } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -8,33 +8,27 @@ import { Label } from '@/components/ui/label';
 import { router } from 'expo-router';
 import { Text } from '@/components/ui/text';
 
-
 interface VerificationCodeProps {
   code: string;
   setCode: (code: string) => void;
 }
 
 export default function VerificationCode() {
-
-  const [code, setCode] = useState<string>('')
+  const [code, setCode] = useState<string>('');
 
   const onChangeCode = () => {
     const digitsOnly = code.replace(/\D/g, '').slice(0, 5);
-    setCode(digitsOnly)
-  }
- 
-  // const onSubmit = () => {
-  //   if (/^\d{6}$/.test(code)) {
-  //     router.push('/(auth)/signup/verification-success');
-  //   } else {
-  //     console.warn('Please enter a valid 6-digit code');
-  //   }
-  // };
+    setCode(digitsOnly);
+  };
+
+  const onSubmit = () => {
+      router.push('/(auth)/forgot-password/create-new-password');
+   
+  };
 
   return (
     <SafeAreaView className="flex-1">
       {/* Hills image as background */}
-      
 
       {/* Card overlay that moves up with keyboard */}
       <KeyboardAvoidingView
@@ -44,53 +38,49 @@ export default function VerificationCode() {
           alignItems: 'center',
           zIndex: 1, // above the image
         }}
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      >
-        <View className="flex-1 px-4  justify-center items-center w-full  ">
-
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <View className="w-full flex-1 items-center justify-center px-4">
           <Card
-            className="border-0 sm:border-border shadow-none sm:shadow-sm sm:shadow-black/5"
-            style={{ zIndex: 2, elevation: 5 }}
-          >
+            className="border-0 shadow-none sm:border-border sm:shadow-sm sm:shadow-black/5"
+            style={{ zIndex: 2, elevation: 5 }}>
             <CardHeader>
-             
-              <CardTitle className="text-center text-primary text-2xl font-semibold sm:text-left">
-               Check your email
-
+              <CardTitle className="text-center text-2xl font-semibold text-primary sm:text-left">
+                Check your email
               </CardTitle>
               <CardDescription className="text-center text-base sm:text-left">
-               Enter the 5-digit code that was mentioned in the email.
+                Enter the 5-digit code that was mentioned in the email.
               </CardDescription>
             </CardHeader>
 
             <CardContent className="gap-6">
               <View className="gap-6">
-                <View className="gap-1.5 flex-row justify-center items-center w-full">
-
+                <View className="w-full flex-row items-center justify-center gap-1.5">
                   <Input
-                  id={code}
-                  keyboardType="numeric"
-                  maxLength={5}
-                  onChangeText={onChangeCode}
+                    id={code}
+                    keyboardType="numeric"
+                    maxLength={5}
+                    onChangeText={onChangeCode}
                     autoCapitalize="none"
                     returnKeyType="next"
-                    className=" h-12  items-center text-2xl tracking-widest text-center text-primary font-semibold"
+                    className="h-12 items-center text-center text-2xl font-semibold tracking-widest text-primary"
                   />
-                  
                 </View>
 
                 <View className="gap-2 pt-1">
-                  <Button >
+                  <Button onPress={onSubmit}>
                     <Text>Verify Code</Text>
                   </Button>
-                 
+
                   <Button
                     variant="outline"
                     className="mx-auto w-full"
-                    onPress={() => router.back()}
-                  >
-                    <Text className=''>Cancel</Text>
-                  </Button> 
+                    onPress={() => router.back()}>
+                    <Text className="">Cancel</Text>
+                  </Button>
+
+                  <Text className="text-center text-base font-normal mt-2 text-muted-foreground">
+                    Didn&apos;t receive the code? <Text className="underline text-primary font-semibold">Resend </Text>
+                  </Text>
                 </View>
               </View>
             </CardContent>
