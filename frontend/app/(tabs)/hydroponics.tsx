2faf4d8857230a8cbfc75cc8f4d119c7e1f5f199
Cel3stt <mariannecelest@gmail.com>
2025-10-20 04:15:52 +0800

    Add hydroponics and change the controls title to filtration in tabs

---
+++ frontend/app/(tabs)/hydroponics.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function Hydroponics() {
+  return (
+     <SafeAreaView className='justify-center items-center flex-1'>
+      <View>
+        <Text>Hydroponics</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
