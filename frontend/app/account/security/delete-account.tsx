49e7b5aeaf3b80dabe63e678f0b8e0966cc013c3
Raymond Palomares <115334271+Mon0629@users.noreply.github.com>
2025-10-21 04:50:58 +0800

    Update delete-account.tsx

--- frontend/app/account/security/delete-account.tsx
+++ frontend/app/account/security/delete-account.tsx
@@ -2,11 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function ChangePassword() {
+export default function Delete() {
   return (
     <SafeAreaView>
       <View>
-        <Text>Change Password</Text>
+        <Text>Delete Account</Text>
       </View>
     </SafeAreaView>
   )
