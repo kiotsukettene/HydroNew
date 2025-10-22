c5a1922403d964600207f2e5993645fa9234b74f
Raymond Palomares <115334271+Mon0629@users.noreply.github.com>
2025-10-23 07:25:55 +0800

    Create reset-password.d.ts

---
+++ frontend/types/reset-password.d.ts
@@ -0,0 +1,15 @@
+interface ResetPasswordStore {
+  loading: boolean;
+  error: string | null;
+  message: string | null;
+  email: string | null;
+  resetToken: string | null;
+  setEmail: (email: string | null) => void;
+  resetPassword: (email: string) => Promise<void>;
+  verifyResetCode: (email: string, otp: string) => Promise<void>;
+  resetPasswordWithToken: (
+    email: string,
+    newPassword: string,
+    resetToken: string
+  ) => Promise<void>;
+}
