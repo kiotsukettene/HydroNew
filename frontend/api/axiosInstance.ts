e27e8d7ff04a99204c3279d75d38be3b248fcebc
Raymond Palomares <115334271+Mon0629@users.noreply.github.com>
2025-10-26 03:01:05 +0800

    Set Content-Type dynamically in axios instance
    Content-Type header is now set to 'application/json' by default, but removed for FormData requests to allow proper handling of multipart data. This ensures correct headers for different request payloads.

--- frontend/api/axiosInstance.ts
+++ frontend/api/axiosInstance.ts
@@ -1,19 +1,21 @@
 import axios from "axios";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const axiosInstance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
-// attach token if exists
 axiosInstance.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (config.data instanceof FormData) {
+    delete config.headers["Content-Type"]; 
+  } else {
+    config.headers["Content-Type"] = "application/json";
+  }
+
   return config;
 });
-
 
 export default axiosInstance;
