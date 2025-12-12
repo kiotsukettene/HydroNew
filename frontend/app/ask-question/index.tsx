835e26193494035f4b3f9e06401c7f81544b41bb
Cel3stt <mariannecelest@gmail.com>
2025-12-13 02:52:20 +0800

    Add Ask Question page with header component

---
+++ frontend/app/ask-question/index.tsx
@@ -0,0 +1,14 @@
+import { PageHeader } from '@/components/ui/page-header'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+       <PageHeader title='Ask your question'/>
+
+      
+        
+    </SafeAreaView>
+  )
+}
