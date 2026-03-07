a5f854654cf5325836e17377f3cee9e119db8764
Cel3stt <mariannecelest@gmail.com>
2026-03-07 11:16:14 +0800

    Refactor FAQ component to improve search input and add keyboard handling

--- frontend/components/ui/input.tsx
+++ frontend/components/ui/input.tsx
@@ -1,15 +1,11 @@
 import { cn } from '@/lib/utils';
 import { Platform, TextInput, type TextInputProps } from 'react-native';
 
-function Input({
-  className,
-  placeholderClassName,
-  ...props
-}: TextInputProps & React.RefAttributes<TextInput>) {
+function Input({ className, ...props }: TextInputProps & React.RefAttributes<TextInput>) {
   return (
     <TextInput
       className={cn(
-        'border-muted-foreground bg-transparent  text-base flex min-h-14 w-full min-w-0 flex-row items-center rounded-xl border px-3 py-2 leading-6 shadow-sm sm:h-12',
+        'dark:bg-input/30 border-input bg-background text-foreground flex h-10 w-full min-w-0 flex-row items-center rounded-md border px-3 py-1 text-base leading-5 shadow-sm shadow-black/5 sm:h-9',
         props.editable === false &&
           cn(
             'opacity-50',
@@ -21,11 +17,10 @@ function Input({
             'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
             'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
           ),
-          native: 'placeholder:text-muted-foreground/50 text-base',
+          native: 'placeholder:text-muted-foreground/50',
         }),
         className
       )}
-      
       {...props}
     />
   );
